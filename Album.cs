39c176d585846a1d35feb2d934c6e709ea01a536
elizabeth hahn <kendoka69@mac.com>
2020-03-16 23:01:15 -0400

    Add StreamReader to read from .csv file.

---
+++ Album.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+public class Album
+{
+	public Album()
+	{
+	}
+}
--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace VinylLibrary
 {
@@ -10,6 +11,19 @@ namespace VinylLibrary
     {
         static void Main(string[] args)
         {
+            string currentDirectory = Directory.GetCurrentDirectory();
+            DirectoryInfo directory = new DirectoryInfo(currentDirectory);
+            var fileName = Path.Combine(directory.FullName, "vinylLibrary.csv");
+            var fileContents = ReadFile(fileName);
+            Console.WriteLine(fileContents);
+        }
+
+        public static string ReadFile(string fileName)
+        {
+            using (var reader = new StreamReader(fileName))
+            {
+                return reader.ReadToEnd();
+            }
         }
     }
 }
