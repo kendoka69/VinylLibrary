ed8ffc85143f3708e6e7c94fa00cf14068f22303
elizabeth hahn <kendoka69@mac.com>
2020-04-26 20:13:01 -0400

    increment Id getter refactored

--- Album.cs
+++ Album.cs
@@ -11,25 +11,18 @@ namespace VinylLibrary
 
     public class Album
     {
-        private static int _Counter;
-        public int ID { get; set; }
+        private static int _counter;
+        public int ID { get; } = Interlocked.Increment(ref _counter);
         public string ArtistName { get; set; }
         public string AlbumTitle { get; set; }
         public string Genre { get; set; }
         public int YearReleased { get; set; }
         public bool OnLoan { get; set; }
         public string Borrower { get; set; }
-        public List<Album> Albums;
-
-        public Album()
-        {
-            Albums = new List<Album>();
-            this.ID = Interlocked.Increment(ref _Counter);
-        }
-
+        
         public override string ToString()
         {
-            return ID + "," + AlbumTitle + ", " + ArtistName + ", " + Genre + ", " + YearReleased + ", " + OnLoan + ", " + Borrower;
+            return $"{ID},{AlbumTitle},{ArtistName},{Genre},{YearReleased},{OnLoan},{Borrower}";
         }
     }
 }
